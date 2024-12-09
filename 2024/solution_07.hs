3a0ea654d5fc8a2c4ab51b1ff60d817da1497422
dannylongeuay <dannylongeuay@yahoo.com>
2024-12-08 20:07:11 -0800

    refactor(2024): :recycle: day 7

--- 2024/solution_07.hs
+++ 2024/solution_07.hs
@@ -1,22 +1,34 @@
+import Data.List (isSuffixOf)
 import Text.Printf
 
 part1 :: [(Int, [Int])] -> Int
-part1 s = sum [x | (x, y) <- s, isValid [(+), (*)] x y]
+part1 s = sum [target | (target, nums) <- s, canSolve target nums False]
 
 part2 :: [(Int, [Int])] -> Int
-part2 s = sum [x | (x, y) <- s, isValid [(+), (*), concatInt] x y]
+part2 s = sum [target | (target, nums) <- s, canSolve target nums True]
 
-concatInt :: Int -> Int -> Int
-concatInt x y = read (show x ++ show y)
-
-isValid :: [Int -> Int -> Int] -> Int -> [Int] -> Bool
-isValid _ _ [] = False
-isValid _ x [y] = x == y
-isValid ops x (y : z : w) = any isValid' ops
+canSolve :: Int -> [Int] -> Bool -> Bool
+canSolve target nums withConcat
+  | length nums == 1 = target == head nums
+  | isDivisible && canSolve nextDividedTarget nextNums withConcat = True
+  | greaterThan && canSolve nextSubtractedTarget nextNums withConcat = True
+  | withConcat
+      && greaterThan
+      && targetEndsWithLast
+      && canSolve nextConcatTarget nextNums withConcat =
+      True
+  | otherwise = False
   where
-    isValid' op = x >= yz && isValid ops x (yz : w)
-      where
-        yz = op y z
+    lastNum = last nums
+    nextNums = init nums
+    isDivisible = mod target lastNum == 0
+    greaterThan = target > lastNum
+    nextDividedTarget = div target lastNum
+    nextSubtractedTarget = target - lastNum
+    targetStr = show target
+    lastStr = show lastNum
+    targetEndsWithLast = lastStr `isSuffixOf` targetStr
+    nextConcatTarget = read $ take (length targetStr - length lastStr) targetStr
 
 parse :: String -> [(Int, [Int])]
 parse s = map f $ lines s
