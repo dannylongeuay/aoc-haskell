3dd4c6ad5f51ac89242eda9725053e492ced0c07
dannylongeuay <dannylongeuay@yahoo.com>
2024-12-11 16:32:04 -0800

    refactor(2024): :recycle: day11 memo solve

--- 2024/solution_11.hs
+++ 2024/solution_11.hs
@@ -1,14 +1,21 @@
 import Data.Bifunctor (first)
 import Data.IntMap qualified as IM
+import Data.MemoTrie (memo2)
 import Text.Printf
 
 type Stones = IM.IntMap Int
 
-part1 :: Stones -> Int
-part1 = solve 25
+-- part1 :: Stones -> Int
+-- part1 = solve 25
 
-part2 :: Stones -> Int
-part2 = solve 75
+part1 :: [Int] -> Int
+part1 = sum . map (`memoSolve` 25)
+
+-- part2 :: Stones -> Int
+-- part2 = solve 75
+
+part2 :: [Int] -> Int
+part2 = sum . map (`memoSolve` 75)
 
 solve :: Int -> Stones -> Int
 solve n stones = sum $ last $ take (n + 1) $ iterate nextStones stones
@@ -30,9 +37,26 @@ applyRule stone
   where
     width = length (show stone)
 
+memoSolve :: Int -> Int -> Int
+memoSolve = memo2 memoSolve'
+
+memoSolve' :: Int -> Int -> Int
+memoSolve' _ 0 = 1
+memoSolve' 0 steps = memoSolve 1 (steps - 1)
+memoSolve' stone steps
+  | even width =
+      let (l, r) = splitAt (div width 2) (show stone)
+       in memoSolve (read l) (steps - 1) + memoSolve (read r) (steps - 1)
+  where
+    width = length (show stone)
+memoSolve' stone steps = memoSolve nextStone (steps - 1)
+  where
+    nextStone = stone * 2024
+
 main :: IO ()
 main = do
   s <- getContents
-  let stones = IM.fromList $ map (\w -> (read w, 1)) $ words s
+  -- let stones = IM.fromList $ map (\w -> (read w, 1)) $ words s
+  let stones = map read $ words s
   printf "Part 1: %d\n" $ part1 stones
   printf "Part 2: %d\n" $ part2 stones
