0d0bda2b8edc484c222a563d6ceb5e9bf5961ebb
dannylongeuay <dannylongeuay@yahoo.com>
2024-12-16 10:17:14 -0800

    refactor(2024): :recycle: day15 simplify part1 and part2

--- 2024/solution_15.hs
+++ 2024/solution_15.hs
@@ -1,152 +1,117 @@
 import Data.Map qualified as M
 import Text.Printf
 
-data Vector = Vector Int Int
-  deriving (Read, Show, Ord, Eq)
+type Vector = (Int, Int)
 
-xVal :: Vector -> Int
-xVal (Vector x _) = x
+vOrigin :: Vector
+vOrigin = (0, 0)
 
-yVal :: Vector -> Int
-yVal (Vector _ y) = y
+vUp :: Vector
+vUp = (0, -1)
+
+vDown :: Vector
+vDown = (0, 1)
+
+vLeft :: Vector
+vLeft = (-1, 0)
+
+vRight :: Vector
+vRight = (1, 0)
+
+(.+.) :: Vector -> Vector -> Vector
+(.+.) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 type Grid = M.Map Vector Char
 
-vUp :: Vector
-vUp = Vector 0 (-1)
+data World = World {grid :: Grid, robotPos :: Vector}
+  deriving (Show)
 
-vDown :: Vector
-vDown = Vector 0 1
-
-vLeft :: Vector
-vLeft = Vector (-1) 0
-
-vRight :: Vector
-vRight = Vector 1 0
+splitByEmpty :: String -> (String, String)
+splitByEmpty s = go s ""
+  where
+    go ('\n' : '\n' : rest) acc = (acc, rest)
+    go (x : xs) acc = go xs (acc ++ [x])
 
 getGrid :: String -> Grid
-getGrid = getGrid' (Vector 0 0)
+getGrid = getGrid' vOrigin
 
 getGrid' :: Vector -> String -> Grid
-getGrid' v ('\n' : '\n' : xs) = M.empty
-getGrid' v ('\n' : xs) = getGrid' (Vector 0 (yVal v + 1)) xs
+getGrid' v [] = M.empty
+getGrid' v ('\n' : xs) = getGrid' (0, snd v + 1) xs
 getGrid' v (x : xs) = M.insert v x grid
   where
     grid = getGrid' (v .+. vRight) xs
 
-getGridDoubleWidth :: String -> Grid
-getGridDoubleWidth = getGridDoubleWidth' (Vector 0 0)
-
-getGridDoubleWidth' :: Vector -> String -> Grid
-getGridDoubleWidth' v ('\n' : '\n' : cs) = M.empty
-getGridDoubleWidth' v ('\n' : cs) = getGridDoubleWidth' (Vector 0 (yVal v + 1)) cs
-getGridDoubleWidth' v (c : cs)
-  | c == '#' = M.insert (v .+. vRight) '#' $ M.insert v '#' grid
-  | c == 'O' = M.insert (v .+. vRight) ']' $ M.insert v '[' grid
-  | c == '@' = M.insert (v .+. vRight) '.' $ M.insert v '@' grid
-  | otherwise = M.insert (v .+. vRight) '.' $ M.insert v '.' grid
-  where
-    grid = getGridDoubleWidth' (v .+. (2 .*. vRight)) cs
-
-(.+.) :: Vector -> Vector -> Vector
-(.+.) (Vector x1 y1) (Vector x2 y2) = Vector (x1 + x2) (y1 + y2)
-
-(.*.) :: Int -> Vector -> Vector
-(.*.) n (Vector x1 y1) = Vector (n * x1) (n * y1)
-
-getMoves :: String -> [Char]
-getMoves ('\n' : '\n' : xs) = [x | x <- xs, x /= '\n']
-getMoves (x : xs) = getMoves xs
+widen :: Char -> [Char]
+widen '#' = "##"
+widen 'O' = "[]"
+widen '.' = ".."
+widen '@' = "@."
+widen '\n' = "\n"
 
 getRobotPos :: Grid -> Vector
 getRobotPos = fst . head . M.assocs . M.filter (== '@')
 
-attemptMove :: Grid -> Vector -> Vector -> [Maybe (Vector, Char)]
-attemptMove g v d
-  | nextC == '#' = [Nothing]
-  | nextC == '.' = [move]
-  | otherwise = move : attemptMove g nextV d
+getWorld :: String -> World
+getWorld s = World {grid, robotPos}
   where
-    nextV = v .+. d
-    nextC = g M.! nextV
-    c = g M.! v
-    move = Just (nextV, c)
-
-moveRobot :: Char -> Vector -> Grid -> (Grid, Vector)
-moveRobot '^' v g = moveRobot' (sequence $ attemptMove g v vUp) v g
-moveRobot '>' v g = moveRobot' (sequence $ attemptMove g v vRight) v g
-moveRobot 'v' v g = moveRobot' (sequence $ attemptMove g v vDown) v g
-moveRobot '<' v g = moveRobot' (sequence $ attemptMove g v vLeft) v g
-
-moveRobot' :: Maybe [(Vector, Char)] -> Vector -> Grid -> (Grid, Vector)
-moveRobot' Nothing v g = (g, v)
-moveRobot' (Just x) v g = (M.union (M.fromList ((v, '.') : x)) g, fst $ head x)
-
-runRobot :: [Char] -> Grid -> Grid
-runRobot moves grid = runRobot' moves robotPos grid
-  where
+    grid = M.filter (/= '.') $ getGrid s
     robotPos = getRobotPos grid
 
-runRobot' :: [Char] -> Vector -> Grid -> Grid
-runRobot' [] _ g = g
-runRobot' (c : cs) v g = runRobot' cs nextV nextG
+getMoves :: String -> [Vector]
+getMoves xs = [moveToDir x | x <- xs, x /= '\n']
+
+moveToDir :: Char -> Vector
+moveToDir '^' = vUp
+moveToDir '>' = vRight
+moveToDir 'v' = vDown
+moveToDir '<' = vLeft
+
+nextVisits :: (Vector, Char) -> Vector -> [Vector] -> [Vector]
+nextVisits (x, c) d q = wideCheck ++ [x .+. d] ++ q
   where
-    (nextG, nextV) = moveRobot c v g
+    wideCheck
+      | snd d /= 0 && c == '[' = [x .+. vRight]
+      | snd d /= 0 && c == ']' = [x .+. vLeft]
+      | otherwise = []
 
-movesToDirs :: String -> [Vector]
-movesToDirs [] = []
-movesToDirs ('^' : xs) = vUp : movesToDirs xs
-movesToDirs ('>' : xs) = vRight : movesToDirs xs
-movesToDirs ('v' : xs) = vDown : movesToDirs xs
-movesToDirs ('<' : xs) = vLeft : movesToDirs xs
-
-sim :: (Grid, Vector) -> Vector -> (Grid, Vector)
-sim (grid, start) d =
-  case go M.empty [start] of
-    Nothing -> (grid, start)
-    Just region -> (grid', start .+. d)
+sim :: World -> Vector -> World
+sim (World grid rPos) d =
+  case go M.empty [rPos] of
+    Nothing -> World grid rPos
+    Just region -> World newGrid (rPos .+. d)
       where
-        grid' =
-          M.union
-            (M.mapKeys (d .+.) region)
-            (M.difference grid region)
+        shiftedRegion = M.mapKeys (d .+.) region
+        prunedGrid = M.difference grid region
+        newGrid = M.union shiftedRegion prunedGrid
   where
-    go seen [] = Just seen
-    go seen (x : xs)
-      | M.notMember x seen,
-        Just c <- M.lookup x grid =
+    go visited [] = Just visited
+    go visited (q : qs)
+      | M.notMember q visited,
+        Just c <- M.lookup q grid =
           if c == '#'
             then Nothing
             else
               go
-                (M.insert x c seen)
-                ( [x .+. vRight | yVal d /= 0, c == '[']
-                    ++ [x .+. vLeft | yVal d /= 0, c == ']']
-                    ++ [x .+. d]
-                    ++ xs
-                )
-      | otherwise = go seen xs
+                (M.insert q c visited)
+                (nextVisits (q, c) d qs)
+      | otherwise = go visited qs
 
-part1 :: Grid -> String -> Int
-part1 startGrid moves = M.foldlWithKey f 0 endGrid
-  where
-    robotPos = getRobotPos startGrid
-    endGrid = runRobot moves startGrid
-    f acc (Vector x y) v = if v == 'O' then acc + x + y * 100 else acc
+gpsScore :: Int -> Vector -> Char -> Int
+gpsScore acc (x, y) c = if c == 'O' || c == '[' then acc + (x + y * 100) else acc
 
-part2 :: Grid -> [Vector] -> Int
-part2 grid moves = M.foldlWithKey f 0 endGrid
-  where
-    robotPos = getRobotPos grid
-    (endGrid, endPos) = foldl sim (grid, robotPos) moves
-    f acc (Vector x y) v = if v == '[' then acc + x + y * 100 else acc
+part1 :: World -> [Vector] -> Int
+part1 world moves = M.foldlWithKey gpsScore 0 $ grid $ foldl sim world moves
+
+part2 :: World -> [Vector] -> Int
+part2 world moves = M.foldlWithKey gpsScore 0 $ grid $ foldl sim world moves
 
 main :: IO ()
 main = do
   s <- getContents
-  let grid = getGrid s
-  let moves = getMoves s
-  let gridDW = M.filter (/= '.') $ getGridDoubleWidth s
-  let moveDirs = movesToDirs moves
-  printf "Part 1: %d\n" $ part1 grid moves
-  printf "Part 2: %d\n" $ part2 gridDW moveDirs
+  let (input1, input2) = splitByEmpty s
+  let world = getWorld input1
+  let moves = getMoves input2
+  let worldExpanded = getWorld $ concatMap widen input1
+  printf "Part 1: %d\n" $ part1 world moves
+  printf "Part 2: %d\n" $ part2 worldExpanded moves
